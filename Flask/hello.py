414ab9e6bee4ccec6abf1dd4674cb2eb3ef8200b
Weiting Tan <wtan12@jhu.edu>
2020-01-12 00:27:00 +0800

    first form finished

--- Flask/hello.py
+++ Flask/hello.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-# from flask.ext.wtf import Form
-# from wtforms import StringField, SubmitField
-# from wtforms.validators import Required
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-#app.config['SECRET_KEY'] = 'hard to guess string'
-
-
-@app.route('/index')
-def index():
-    return render_template('index.html')
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 @app.route('/user/<name>')
 def user(name):
@@ -26,18 +21,18 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-# class NameForm(Form):
-#     name = StringField('What is your name?', validators=[Required()])
-#     submit = SubmitField('Submit')
-#
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     name = None
-#     form = NameForm()
-#     if form.validate_on_submit():
-#     name = form.name.data
-#     form.name.data = ''
-#     return render_template('index.html', form=form, name=name)
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 
