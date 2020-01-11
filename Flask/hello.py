7cb3e14f4a6ad012edfe04302ae15bc77cf8ba5b
Weiting Tan <wtan12@jhu.edu>
2020-01-12 00:58:29 +0800

    update flash message

--- Flask/hello.py
+++ Flask/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
@@ -25,16 +25,18 @@ class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
-
-
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            #flash status update message for user
+            flash("Looks like you have changed your name")
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
     app.run(debug=True)
