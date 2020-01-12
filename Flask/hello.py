499b403adede1cadc4cce6f8563573ba407df960
Weiting Tan <wtan12@jhu.edu>
2020-01-12 15:36:25 +0800

    database fully functional

--- Flask/hello.py
+++ Flask/hello.py
@@ -1,12 +1,48 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager, Shell
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from wtforms.validators import Required
 
+#Configurations
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'mysql+mysqldb://root:twt123456@localhost:3306/sqlalchemy'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+#utilize bootstrap and mysql
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+manager = Manager(app)
+
+#Form definition
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+#Model definition:
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    def __repr__(self):
+        return '<Role %r>' % self.name
+    users = db.relationship('User', backref='role',lazy='dynamic')
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    def __repr__(self):
+        return '<User %r>' % self.username
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+
+
 
 @app.route('/user/<name>')
 def user(name):
@@ -21,25 +57,37 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-class NameForm(Form):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            #flash status update message for user
-            flash("Looks like you have changed your name")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
+        # old_name = session.get('name')
+        # if old_name is not None and old_name != form.name.data:
+        #     #flash status update message for user
+        #     flash("Looks like you have changed your name")
+        # session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html',
+                           form=form,
+                           name=session.get('name'),
+                           known=session.get('known', False))
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
 
 
 
