e6689d72ab64b4d27a06ca3ae4d8bbf451b35ebd
Weiting Tan <wtan12@jhu.edu>
2020-01-12 17:54:38 +0800

    send email function finished

--- Flask/hello.py
+++ Flask/hello.py
@@ -4,8 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager, Shell
 from flask_wtf import Form
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from threading import Thread
+import os
 from wtforms.validators import Required
 
 #Configurations
@@ -15,11 +18,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
     'mysql+mysqldb://root:twt123456@localhost:3306/sqlalchemy'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #suppress warning
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <steventan0110@gmail.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 #utilize bootstrap and mysql
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 manager = Manager(app)
+mail = Mail(app)
 
 #DB migration
 migrate = Migrate(app,db)
@@ -61,6 +73,21 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+#background thread sending emails:
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'],
+                  recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    print(msg.body,msg.html)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -71,6 +98,9 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
 
