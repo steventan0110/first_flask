bbf9ca337ecd91631657cb16ba461fa525122bc1
Weiting Tan <wtan12@jhu.edu>
2020-01-13 14:15:28 +0800

    update login authentication form

--- Flask/app/models.py
+++ Flask/app/models.py
@@ -1,4 +1,11 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -8,10 +15,30 @@ class Role(db.Model):
         return '<Role %r>' % self.name
     users = db.relationship('User', backref='role',lazy='dynamic')
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
+
+    #column attributes:
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))    #foreign key to roles
+    password_hash = db.Column(db.String(128))
+
     def __repr__(self):
         return '<User %r>' % self.username
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+
+
+
+
+    @property
+    def password(self):
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash,password)
